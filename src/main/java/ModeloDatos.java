b6f5e71afb9ca490dc202b93e77e55ac6aca7b47
liu-hanli <hanli.liu@edu.uah.es>
2024-02-23 22:42:08 +0100

    Fix sonar major issues

--- src/main/java/ModeloDatos.java
+++ src/main/java/ModeloDatos.java
@@ -1,10 +1,12 @@
 import java.sql.*;
+import java.util.logging.Logger;
 
 public class ModeloDatos {
 
     private Connection con;
     private Statement set;
     private ResultSet rs;
+    private Logger logger = Logger.getLogger("ModeloDatos");
 
     public void abrirConexion() {
 
@@ -23,8 +25,8 @@ public class ModeloDatos {
 
         } catch (Exception e) {
             // No se ha conectado
-            System.out.println("No se ha podido conectar");
-            System.out.println("El error es: " + e.getMessage());
+            logger.severe("No se ha podido conectar");
+            logger.severe("El error es: " + e.getMessage());
         }
     }
 
@@ -45,8 +47,8 @@ public class ModeloDatos {
             set.close();
         } catch (Exception e) {
             // No lee de la tabla
-            System.out.println("No lee de la tabla");
-            System.out.println("El error es: " + e.getMessage());
+            logger.severe("No lee de la tabla");
+            logger.severe("El error es: " + e.getMessage());
         }
         return (existe);
     }
@@ -59,8 +61,8 @@ public class ModeloDatos {
             set.close();
         } catch (Exception e) {
             // No modifica la tabla
-            System.out.println("No modifica la tabla");
-            System.out.println("El error es: " + e.getMessage());
+            logger.severe("No modifica la tabla");
+            logger.severe("El error es: " + e.getMessage());
         }
     }
 
@@ -72,8 +74,8 @@ public class ModeloDatos {
             set.close();
         } catch (Exception e) {
             // No inserta en la tabla
-            System.out.println("No inserta en la tabla");
-            System.out.println("El error es: " + e.getMessage());
+            logger.severe("No inserta en la tabla");
+            logger.severe("El error es: " + e.getMessage());
         }
     }
 
@@ -81,7 +83,7 @@ public class ModeloDatos {
         try {
             con.close();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            logger.severe(e.getMessage());
         }
     }
 
